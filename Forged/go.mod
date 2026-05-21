fe9b012be26eaa29aae2375377d330d8a99a7d0e
ochuluemmanuella-blip <ochuluemmanuella@gmail.com>
2026-05-21 16:39:08 +0100

    clash

---
+++ Forged/go.mod
@@ -0,0 +1,3 @@
+module Forged
+
+go 1.22.2
---
+++ Forged/internal/beat/beat.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Beat struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Genre       string `json:"genre"`
+	Mood        string `json:"mood"`
+	BPM         int    `json:"bpm"`
+	Description string `json:"description"`
+}
+
+var genres = []string{
+	"Trap", "Afrobeats", "Hip-hop", "R&B", "Pop", "Phonk",
+}
+
+var moods = []string{
+	"Dark", "Nostalgic", "Chill", "Emotional", "Hype",
+	"Energetic", "Mysterious",
+}
+
+var prefixes = []string{
+	"Shadow", "Velvet", "Ocean", "Crystal",
+	"Starlight",
+}
+
+var suffixes = []string{
+	"Chaos", "Soul", "Dreams", "Love", "Nights",
+	"Echo", "Vibes",
+}
+
+func GenerateBeat() Beat {
+	rand.Seed(time.Now().UnixNano())
+	genre := genres[rand.Intn(len(genres))]
+	mood := moods[rand.Intn(len(moods))]
+	prefix := prefixes[rand.Intn(len(prefixes))]
+	suffix := suffixes[rand.Intn(len(suffixes))]
+
+	name := prefix + " " + suffix
+
+}
